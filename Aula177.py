b880b82521ec0223075fc16dabfb05851d58f7ac
tabaxco <rodriguesgui48@gmail.com>
2025-12-16 12:53:20 -0300

    Aula 177

---
+++ Aula177.py
@@ -0,0 +1,32 @@
+from itertools import groupby
+
+alunos = [
+    {'nome': 'Luiz', 'nota':'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabricio', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'}
+]
+
+def ordena(aluno):
+    return aluno['nota']
+
+#alunos_agrupados = sorted(alunos, key=ordena)
+#grupos = groupby(alunos_agrupados, key=ordena)
+
+alunos_agrupados = sorted(alunos, key=lambda a: a['nota'])
+grupos = groupby(alunos_agrupados, key=lambda a: a['nota'])
+
+
+#alunos = ['a', 'a', 'a', 'a' 'b', 'c']
+#grupos = groupby(alunos)
+#grupos = groupby(sorted(alunos))
+
+for chave, grupo in grupos:
+    print(chave)
+    for aluno in grupo:
+        print(aluno)
