7083109c7aa7479b30607ec6505838ded8d23038
tabaxco <rodriguesgui48@gmail.com>
2025-12-17 13:39:23 -0300

    Aula 189

---
+++ aula189.py
@@ -0,0 +1,25 @@
+caminho_arquivo = '/home/tabaxco/Projetos/Luis_Otavio_Python3/'
+caminho_arquivo += 'aula189.txt'
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+    arquivo.seek(0,0)
+    print(arquivo.read())
+    arquivo.seek(0,0)
+    print(arquivo.readline())
+    print(arquivo.readline().strip())
+    print(arquivo.readline(), end = '')
+
+    arquivo.seek(0,0)
+    for linha in arquivo.readlines():
+            print(linha.strip())
+    
+
+print('#' * 40)
+
+with open(caminho_arquivo,'r') as arquivo:
+    print(arquivo.read())
