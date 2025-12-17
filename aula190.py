044d69cfdfb2a4e93a3d2cf8813f9cfa13c6d210
tabaxco <rodriguesgui48@gmail.com>
2025-12-17 14:43:27 -0300

    Aula 190

---
+++ aula190.py
@@ -0,0 +1,5 @@
+caminho_arquivo = '/home/tabaxco/Projetos/Luis_Otavio_Python3/'
+caminho_arquivo += 'aula190.txt'
+
+with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
+    arquivo.write('Atenção') 
