ce138a476b1a3e9b98134b8350ae4bafefa774bf
tabaxco <rodriguesgui48@gmail.com>
2025-12-18 15:11:49 -0300

    193

--- aula193.py
+++ aula193.py
@@ -1,9 +1,14 @@
-def adiciona_clientes(nome, lista=[]):
+def adiciona_clientes(nome, lista=None):
+    if lista is None:
+        lista = []
     lista.append(nome)
     return lista
 
 cliente1=adiciona_clientes('guilherme')
 adiciona_clientes('Joana', cliente1)
+
 cliente2=adiciona_clientes('macaco')
+adiciona_clientes('Miquinho', cliente2)
 
 print(cliente1)
+print(cliente2)
